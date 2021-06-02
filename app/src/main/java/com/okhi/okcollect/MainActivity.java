af3a8617a08887f118181ebcb8333c6aef0fded3
Julius Kiano <jgkiano@gmail.com>
2021-06-02 08:20:17 +0300

    use new auth structure

--- app/src/main/java/com/okhi/okcollect/MainActivity.java
+++ app/src/main/java/com/okhi/okcollect/MainActivity.java
@@ -5,96 +5,70 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.PowerManager;
-import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import io.okhi.android_core.OkHi;
 import io.okhi.android_core.interfaces.OkHiRequestHandler;
-import io.okhi.android_core.models.OkHiAppContext;
-import io.okhi.android_core.models.OkHiAuth;
-import io.okhi.android_core.models.OkHiDeveloperType;
 import io.okhi.android_core.models.OkHiException;
 import io.okhi.android_core.models.OkHiLocation;
-import io.okhi.android_core.models.OkHiMode;
-import io.okhi.android_core.models.OkHiPlatformType;
 import io.okhi.android_core.models.OkHiUser;
 import io.okhi.android_okcollect.OkCollect;
 import io.okhi.android_okcollect.callbacks.OkCollectCallback;
 import io.okhi.android_okcollect.utilities.OkHiConfig;
 import io.okhi.android_okcollect.utilities.OkHiTheme;
 
-import static com.okhi.okcollect.Secret.DEV_CLIENT_BRANCH;
-import static com.okhi.okcollect.Secret.DEV_CLIENT_KEY;
-import static com.okhi.okcollect.Secret.OKHI_MODE;
 import static com.okhi.okcollect.Secret.TEST_PHONE;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final OkHiAppContext okhiAppContext = new OkHiAppContext.Builder(OKHI_MODE)
-            .setDeveloper(OkHiDeveloperType.OKHI)
-            .setPlatform(OkHiPlatformType.ANDROID)
-            .setAppMeta("OkHi", "1.0.0", 1)
-            .build();
-
-    private static final OkHiAuth okhiAuth = new OkHiAuth.Builder(DEV_CLIENT_BRANCH, DEV_CLIENT_KEY)
-            .withContext(okhiAppContext)
-            .build();
-
     private OkCollect okCollect;
     private OkHi okhi;
     private Button launchBtn;
+    private OkHiTheme theme = new OkHiTheme.Builder("#00fdaa")
+        .setAppBarLogo("https://cdn.okhi.co/icon.png")
+        .setAppBarColor("#ba0c2f")
+        .build();
+    private OkHiConfig config = new OkHiConfig.Builder()
+        .withStreetView()
+        .build();
+    private OkHiUser user = new OkHiUser.Builder(TEST_PHONE)
+        .withFirstName("Julius")
+        .withLastName("Kiano")
+        .build();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         launchBtn = findViewById(R.id.launchBtn);
-        okhi = new OkHi(this);
-
-        OkHiTheme theme = new OkHiTheme.Builder("#00fdaa")
-                .setAppBarLogo("https://cdn.okhi.co/icon.png")
-                .setAppBarColor("#ba0c2f")
-                .build();
-
-        OkHiConfig config = new OkHiConfig.Builder()
-                .withStreetView()
-                .build();
-
-        okCollect = new OkCollect.Builder(okhiAuth, this)
+        try {
+            okhi = new OkHi(this);
+            okCollect = new OkCollect.Builder(this)
                 .withTheme(theme)
                 .withConfig(config)
                 .build();
+        } catch (OkHiException exception) {
+            exception.printStackTrace();
+        }
     }
 
     private void launchOkCollect(){
         boolean canStartOkCollect = canStartAddressCreation();
         if(canStartOkCollect) {
-            final OkHiUser user = new OkHiUser.Builder(TEST_PHONE)
-                    .withFirstName("Ramogi")
-                    .withLastName("Ochola")
-                    .build();
             okCollect.launch(user, new OkCollectCallback<OkHiUser, OkHiLocation>() {
                 @Override
                 public void onSuccess(OkHiUser user, OkHiLocation location) {
                     showMessage(user.getPhone() + " " + location.getId());
                 }
-
                 @Override
                 public void onError(OkHiException e) {
                     showMessage(e.getCode() + " " + e.getMessage());
                 }
             });
-
         }
     }
 
@@ -134,22 +108,21 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        // Pass permission results to okverify
+        // Pass permission results to okhi
         okhi.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
-        // Pass activity results results to okverify
+        // Pass activity results results to okhi
         okhi.onActivityResult(requestCode, resultCode, data);
     }
 
     public void handleButtonTap(View view) {
         launchOkCollect();
     }
+
     private void displayLog(String log){
         Log.i("MainActivity", log);
     }
