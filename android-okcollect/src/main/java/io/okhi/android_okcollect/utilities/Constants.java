33f4550d8533568c5107611af9eb663e9b93c584
Julius Kiano <jgkiano@gmail.com>
2022-09-07 15:05:35 +0300

    change to verify token

--- android-okcollect/src/main/java/io/okhi/android_okcollect/utilities/Constants.java
+++ android-okcollect/src/main/java/io/okhi/android_okcollect/utilities/Constants.java
@@ -3,7 +3,7 @@ package io.okhi.android_okcollect.utilities;
 import io.okhi.android_core.models.OkHiAccessScope;
 
 public class Constants {
-    public static final String[] SCOPES = {OkHiAccessScope.ADDRESS};
+    public static final String[] SCOPES = {OkHiAccessScope.VERIFY};
     public static final String PROD_HEART_URL = "https://manager-v5.okhi.io";
     public static final String SANDBOX_HEART_URL = "https://sandbox-manager-v5.okhi.io";
     public static final String DEV_HEART_URL = "https://dev-manager-v5.okhi.io";
