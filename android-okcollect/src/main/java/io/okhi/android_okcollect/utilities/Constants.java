aedc5c1fa2c50feb671f01be65cf713be93adc69
Julius Kiano <jgkiano@gmail.com>
2023-02-07 13:25:11 +0300

    added legacy values

--- android-okcollect/src/main/java/io/okhi/android_okcollect/utilities/Constants.java
+++ android-okcollect/src/main/java/io/okhi/android_okcollect/utilities/Constants.java
@@ -5,14 +5,14 @@ import io.okhi.android_core.models.OkHiAccessScope;
 public class Constants {
     public static final String[] SCOPES = {OkHiAccessScope.VERIFY};
     //Prod
-    public static final String PROD_HEART_URL_POST_21 = "https://manager-v5.okhi.io";
-    public static final String PROD_HEART_URL_PRE_21 = "https://google.com";
+    public static final String PROD_HEART_URL_POST_22 = "https://manager-v5.okhi.io";
+    public static final String PROD_HEART_URL_PRE_22 = "https://legacy-manager-v5.okhi.io";
 
     // Sandbox
-    public static final String SANDBOX_HEART_URL_POST_21 = "https://sandbox-manager-v5.okhi.io";
-    public static final String SANDBOX_HEART_URL_PRE_21 =  "https://google.com";
+    public static final String SANDBOX_HEART_URL_POST_22 = "https://sandbox-manager-v5.okhi.io";
+    public static final String SANDBOX_HEART_URL_PRE_22 =  "https://sandbox-legacy-manager-v5.okhi.io";
 
     //Dev
-    public static final String DEV_HEART_URL_POST_21 = "https://dev-manager-v5.okhi.io";
-    public static final String DEV_HEART_URL_PRE_21 = "https://google.com";
+    public static final String DEV_HEART_URL_POST_22 = "https://dev-manager-v5.okhi.io";
+    public static final String DEV_HEART_URL_PRE_22 = "https://dev-legacy-manager-v5.okhi.io";
 }
